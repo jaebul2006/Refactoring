b78fbea4b6ff648dc7b20414b32a17da3422d6ea
dh.jang <dh.jang@thehrder.com>
2022-05-03 11:19:27 +0900

    조건문 다형성을 위한 타입에 따라 다른 서브클래스 생성 팩토리 함수추가 및 마무리

--- CreateStatementData.js
+++ CreateStatementData.js
@@ -8,35 +8,54 @@ class PerformanceCalculator
 
     get Amount()
     {
-        let result = 0;
-        switch(this.play.type)
-        {
-            case "tragedy":
-                result = 40000;
-                if(this.performance.audience > 30){
-                    result += 1000 * (this.performance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if(this.performance.audience > 20){
-                    result += 10000 + 500 * (this.performance.audience - 20);
-                }
-                result += 300 * this.performance.audience;
-                break;
-            default:
-                throw new Error(`알 수 없는 장르: ${this.play.type}`);
+        throw new Error(`서브클래스에서 처리하도록 설계됨`);
+    }
+
+    get VolumeCredits()
+    {
+        return Math.max(this.performance.audience - 30, 0);
+    }
+}
+
+class TragedyCalculator extends PerformanceCalculator
+{
+    get Amount()
+    {
+        let result = 40000;
+        if(this.performance.audience > 30){
+            result += 1000 * (this.performance.audience - 30);
         }
+        return result;
+    }
+}
+
+class ComedyCalculator extends PerformanceCalculator
+{
+    get Amount()
+    {
+        let result = 30000;
+        if(this.performance.audience > 20){
+            result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
         return result;
     }
 
     get VolumeCredits()
     {
-        let result = 0;
-        result += Math.max(this.performance.audience - 30, 0);
-        if("comedy" == this.play.type)
-        result += Math.floor(this.performance.audience / 5);
-        return result;
+        return super.VolumeCredits + Math.floor(this.performance.audience / 5);
+    }
+}
+
+// 자바스크립트에서는 생성자가 서브클래스의 인스턴스를 반환할 수 없어서 만든 함수
+function CreatePerformanceCalculator(aPerformance, aPlay)
+{
+    switch(aPlay.type)
+    {
+        case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+        case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+        default:
+            throw new Error(`알 수 없는 장르: ${aPlay.type}`);
     }
 }
 
@@ -51,7 +70,7 @@ export default function CreateStatementData(invoice, plays)
 
     function EnrichPerformance(aPerformance)
     {
-        const calculator = new PerformanceCalculator(aPerformance, Playfor(aPerformance));
+        const calculator = CreatePerformanceCalculator(aPerformance, Playfor(aPerformance));
         const result = Object.assign({}, aPerformance);
         result.play = calculator.play;
         result.amount = calculator.Amount;
